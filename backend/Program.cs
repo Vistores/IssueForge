858fc41a7a6ee3afaefc784d1f38e4581720d8fb
Vistores <151128816+Vistores@users.noreply.github.com>
2026-05-11 15:43:29 +0300

    initialize repository structure

---
+++ backend/Program.cs
@@ -0,0 +1,50 @@
+using GameIssueTracker.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularClient", policy =>
+        policy
+            .WithOrigins("http://localhost:4200", "https://localhost:4200", "http://127.0.0.1:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+    await DbSeeder.SeedAsync(db);
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AngularClient");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
